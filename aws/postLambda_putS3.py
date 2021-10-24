443d15058b079184aa3401ee7e349b4775732379
Yuji Tokiwa <tokiwa@yujitokiwa.jp>
2021-10-24 18:39:32 +0900

    Real URL in sample file

---
+++ aws/postLambda_putS3.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+from datetime import datetime
+
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+    body = event["body"]
+    print(body)
+    bucket = 'yujitokiwa-jp-test'
+    key = 'post_' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.txt'
+    file_contents = body
+    obj = s3.Object(bucket,key)
+    obj.put( Body=file_contents )
+    return {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'     
+        },
+        'body': '{"message": "s3 put works"}'
+    }
